45e7ad99ddb9314e97657aef420e73d24d58489a
heleven <djhuang@DJdeMacBook-Pro.local>
2019-06-03 09:59:01 +0800

    add socket component

--- src/main.js
+++ src/main.js
@@ -5,6 +5,21 @@ import Vue from 'vue';
 import App from './App';
 import store from './store';
 
+import VueSocketIO from 'vue-socket.io'
+
+const options = { path: '/my-app/' }; //Options object to pass into SocketIO
+
+Vue.use(new VueSocketIO({
+    debug: true,
+    connection: SocketIO('http://metinseylan.com:1992', options), //options object is Optional
+    vuex: {
+      store,
+      actionPrefix: "SOCKET_",
+      mutationPrefix: "SOCKET_"
+    }
+  })
+);
+
 Vue.config.devtools = true;
 
 new Vue({
